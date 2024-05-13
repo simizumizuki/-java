0bb97e2c0264373bcde47c553595f0d6661062f7
mizuki <zeal23418011@fsgcl.jp>
2024-05-13 11:58:06 +0900

    syokyuujava

---
+++ 初級java2.5/src/start.java
@@ -0,0 +1,16 @@
+
+public class start {
+
+	public static void main(String[] args) {
+		MyFrame frame1 = new MyFrame(); 
+		frame1.setLocation(200,150);
+		frame1.setSize(150,150);
+		MyFrame frame2 = new MyFrame();
+		frame2.setLocation(350,200);
+		MyFrame frame3 = new RectFrame();
+		frame3.setLocation(50,300);
+		MyFrame frame4 = new RectFrame();
+		frame3.setLocation(100,300);
+	}
+
+}
---
+++ 初級java2.5/src/練習.java
@@ -0,0 +1,10 @@
+
+public class 練習 {
+
+	public static void main(String[] args) {
+		int x = 2;
+		int y = 3;
+		System.out.println(x*y);
+	}
+
+}
