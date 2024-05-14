a066ee6fd0b04e018e5751e4f835040510252f0e
mizuki <zeal23418011@fsgcl.jp>
2024-05-14 12:18:17 +0900

    3.4

---
+++ 初級java2.5/src/OvalTest.java
@@ -0,0 +1,8 @@
+
+public class OvalTest extends MyFrame{
+	public void run() {
+		fillOval(30,40,50,100);
+		fillOval(100,40,50,100);
+	}
+
+}
--- 初級java2.5/src/start.java
+++ 初級java2.5/src/start.java
@@ -3,7 +3,7 @@ public class start {
 
 	public static void main(String[] args) {
 		System.out.println("プログラムを実行します");
-		MyFrame frame1 = new RectTest(); 
+		MyFrame frame1 = new OvalTest(); 
 		
 	}
 
