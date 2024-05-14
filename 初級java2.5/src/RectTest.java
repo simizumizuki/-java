85acd5a71eea3a9fe9fc77ba67d71c9612cb8234
mizuki <zeal23418011@fsgcl.jp>
2024-05-14 12:10:35 +0900

    3.3

--- 初級java2.5/src/RectTest.java
+++ 初級java2.5/src/RectTest.java
@@ -1,9 +1,15 @@
 
 public class RectTest  extends MyFrame{
 public void run() {
+	setColor(250,250,0);
 	fillRect(30,40,50,100);
+	setColor(200,0,255);
 	fillRect(130,40,50,100);
-	fillRect(30,200,150,100);
-	fillRect(90,160,30,30);
+	setColor(0,125,200);
+	fillRect(30,60,150,10);
+	setColor(0,0,0);
+	fillRect(30,80,150,10);
+	setColor(128,128,128);
+	fillRect(30,70,150,10);
 }
 }
