a613d168dec0f49e0fce6deb82fe64cd06761003
mizuki <zeal23418011@fsgcl.jp>
2024-05-20 12:14:16 +0900

    4/4

--- 初級java2.5/src/RectIf.java
+++ 初級java2.5/src/RectIf.java
@@ -5,8 +5,10 @@ public class RectIf extends MyFrame{
 		int x = 30;
 		int i;
 		for(i=0;i<9;i++) {
-			if(i>=4&&i<=7) {
+			if(i==8) {
 				fillRect(x,100,10,100);
+			}else if(i==0){
+				fillRect(x,60,10,100);
 			}else {
 				fillRect(x,80,10,100);
 			}
