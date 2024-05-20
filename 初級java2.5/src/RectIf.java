24370cfed0ee8ed32822eb709be37a574ab7494e
mizuki <zeal23418011@fsgcl.jp>
2024-05-20 12:09:32 +0900

    4.4

---
+++ 初級java2.5/src/RectIf.java
@@ -0,0 +1,18 @@
+
+public class RectIf extends MyFrame{
+	public void run() {
+		setColor(0, 128, 0);
+		int x = 30;
+		int i;
+		for(i=0;i<9;i++) {
+			if(i>=4&&i<=7) {
+				fillRect(x,100,10,100);
+			}else {
+				fillRect(x,80,10,100);
+			}
+			
+			x+=20;
+		}
+	}
+	
+}
