ac14b1a1c7f33b2e132792e07d76c2404d0314d2
mizuki <zeal23418011@fsgcl.jp>
2024-05-27 10:04:13 +0900

    5.3

--- 初級java2.5/src/Animation.java
+++ 初級java2.5/src/Animation.java
@@ -1,14 +1,16 @@
 
-public class DisplayRectsSlow extends MyFrame{
+public class Animation extends MyFrame{
 	public void run() {
-		setColor(0,128,0);
 		int x = 30;
 		while(true) {
 			clear();
 			setColor(0,128,0);
 			fillRect(x,80,10,100);
-			x+=20;
-			sleep(1);
+			x+=5;
+			if(x>200) {
+				break;
+			}
+			sleep(0.1);
 		}
 	}
 
