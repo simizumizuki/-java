41f181851c2470836449ee840f5e831dadd65c42
mizuki <zeal23418011@fsgcl.jp>
2024-05-27 10:45:11 +0900

    5.3.5

--- 初級java2.5/src/Animation.java
+++ 初級java2.5/src/Animation.java
@@ -2,14 +2,20 @@
 public class Animation extends MyFrame{
 	public void run() {
 		int x = 30;
-		while(true) {
+		int y = 100;
+		while(x<=170) {
 			clear();
 			setColor(0,128,0);
-			fillRect(x,80,10,100);
+			fillRect(x,y,10,100);
 			x+=5;
-			if(x>200) {
-				break;
-			}
+			sleep(0.1);
+		}
+		while(y<=200) {
+			clear();
+			setColor(0,128,0);
+			fillRect(x,y,10,100);
+			
+			y+=5;
 			sleep(0.1);
 		}
 	}
