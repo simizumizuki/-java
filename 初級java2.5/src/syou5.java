adf891ec3f96f096017fbafc43434402c1e88495
mizuki <zeal23418011@fsgcl.jp>
2024-05-27 11:03:30 +0900

    5-3.

--- 初級java2.5/src/syou5.java
+++ 初級java2.5/src/syou5.java
@@ -3,36 +3,22 @@ public class syou5 extends MyFrame{
 	public void run() {
 		int x = 50;
 		int y = 50;
-		while(x<=100) {
-			clear();
-			setColor(0,128,0);
-			fillRect(x,y,10,100);
-			x+=5;
-			y+=5;
-			sleep(0.1);
-		}
-		while(x>=50) {
-			clear();
-			setColor(0,128,0);
-			fillRect(x,y,10,100);
-			x-=5;
-			sleep(0.1);
-		}
-		while(x<=100) {
-			clear();
-			setColor(0,128,0);
-			fillRect(x,y,10,100);
-			x+=5;
-			y-=5;
-			sleep(0.1);
-		}
-		while(x>=50) {
-			clear();
-			setColor(0,128,0);
-			fillRect(x,y,10,100);
-			x-=5;
-			sleep(0.1);
+		for(int i = 0;i<3;i++) {
+			while(x<=170) {
+				clear();
+				setColor(0,128,0);
+				fillRect(x,y,10,100);
+				x+=5;
+				sleep(0.1);
+			}
+			while(x>=50) {
+				clear();
+				setColor(0,128,0);
+				fillRect(x,y,10,100);
+				x-=5;
+				sleep(0.1);
+			}
 		}
 	}
-
 }
+
