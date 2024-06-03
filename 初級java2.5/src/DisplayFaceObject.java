c216ce5333af99c3e4431de230cff08627d2a4f3
mizuki <zeal23418011@fsgcl.jp>
2024-06-03 10:44:18 +0900

    7-4

--- 初級java2.5/src/DisplayFaceObject.java
+++ 初級java2.5/src/DisplayFaceObject.java
@@ -1,19 +1,19 @@
 
 public class DisplayFaceObject extends MyFrame{
 	public void run() {
-		int x = 30;
-		Face face1 = new Face(50,50,10,5);
-		Face face2 = new Face(200,100,-10,-5);
-		Face face3 = new Face(0,100,5,0);
+		Face[] faces=new Face[5];
+		 faces[0] = new Face(50,50,10,5);
+		 faces[1]= new Face(200,100,-10,-5);
+		 faces[2]= new Face(0,100,5,0);
+		 faces[3]= new Face(200,100,-5,0);
+		 faces[3]= new Face(100,0,0,5);
 		for(int i =0;i<30;i++) {
 			clear();
-			face1.draw(this);
-			face2.draw(this);
-			face3.draw(this);
-			face1.move();
-			face2.move();
-			face3.move();
-;			sleep(0.1);
+		for(int j = 0;j<faces.length;j++) {
+			faces[j].draw(this);
+			faces[j].move();
+		}
+		sleep(0.1);
 		}
 	}
 
