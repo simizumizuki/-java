2563db17736be0ad486cb50e1c3b12057d8d71b2
mizuki <zeal23418011@fsgcl.jp>
2024-06-11 10:15:44 +0900

    9.2

--- 初級java2.5/src/Car.java
+++ 初級java2.5/src/Car.java
@@ -1,5 +1,7 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Car extends Vehicle{
+public class Car extends Vehicle implements KeyListener{
 public Car(int x, int y, int vx, int vy) {
 		super(x, y, vx, vy);
 		// TODO 自動生成されたコンストラクター・スタブ
@@ -10,6 +12,37 @@ public void draw(MyFrame frame) {
 	frame.fillRect(x, y+20, 80, 20);
 	frame.fillOval(x+10, y+40, 20, 20);
 	frame.fillOval(x+50, y+40, 20, 20);
+}
+
+@Override
+public void keyTyped(KeyEvent e) {
+	
+	// TODO 自動生成されたメソッド・スタブ
+	
+}
+
+@Override
+public void keyPressed(KeyEvent e) {
+	if(e.getKeyCode()==KeyEvent.VK_LEFT) {
+		vx-=5;
+	}
+	if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
+		vx+=5;
+	}
+	// TODO 自動生成されたメソッド・スタブ
+	
+}
+
+@Override
+public void keyReleased(KeyEvent e) {
+	if(e.getKeyCode()==KeyEvent.VK_LEFT) {
+		vx=0;
+	}
+	if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
+		vx=0;
+	}
+	// TODO 自動生成されたメソッド・スタブ
+	
 }
 
 }
